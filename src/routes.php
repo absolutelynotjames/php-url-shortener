d5b929631922a28ed6c1e200927de5348292bfd9
James Myers <james.myers@msn.com>
2017-12-04 05:52:00 -0500

    initial random number generator for ids

--- src/routes.php
+++ src/routes.php
@@ -4,14 +4,36 @@ use Slim\Http\Request;
 use Slim\Http\Response;
 
 // Routes
-
 $app->get('/', function (Request $request, Response $response, array $args) {
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
 
 $app->post('/new', function (Request $request, Response $response, array $args) {
-    // Sample log message
-    $this->logger->info($request->getBody());
-    // Render index view
+
+    //get post body
+    $submission = $request->getParsedBody();    
+
+    //random number generator for unique id
+    $randomFactory = new RandomLib\Factory;
+    $idGenerator = $randomFactory->getLowStrengthGenerator();    
+
+    //let's keep our character set simple
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    //for timestamping submissions, might be useful for something later on
+    $time = Time();
+
+    //generate inique id
+    $id = $idGenerator->generateString(8, $characters);
+
+    $this->logger->info($submission['url']);
+    
+    try {
+        $this->db->set($id, ['url' => $submission['url'], 'time' => $time]);
+    } catch(Exception $e) {
+        $this->logger->info($e);
+    } finally {
+        return $id;
+    }
 });
