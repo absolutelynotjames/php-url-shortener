330b4559b7764f2e49ed328d0eeeedf6e0f48b91
James Myers <james.myers@msn.com>
2017-12-04 06:58:17 -0500

    Short url redirect route

--- src/routes.php
+++ src/routes.php
@@ -3,9 +3,25 @@
 use Slim\Http\Request;
 use Slim\Http\Response;
 
-// Routes
+
 $app->get('/', function (Request $request, Response $response, array $args) {
     // Render index view
+    return $this->renderer->render($response, 'index.phtml', $args);
+});
+
+//pattern for matching against our url generator
+$app->get('/{id:[0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ]{8}}', function (Request $request, Response $response, array $args) {
+
+    //get key from url
+    $key = $this->db->get($args['id']);
+
+    //if db returns something for this key
+    if ($key):
+        return $response->withRedirect($key['url']);         
+    else:
+        return $response->withRedirect('/');                 
+    endif;
+
     return $this->renderer->render($response, 'index.phtml', $args);
 });
 
@@ -27,8 +43,14 @@ $app->post('/new', function (Request $request, Response $response, array $args) 
     //generate inique id
     $id = $idGenerator->generateString(8, $characters);
 
+    //make sure id isn't already in the database
+    while ($this->db->get($id) !== false):
+        $id = $idGenerator->generateString(8, $characters);        
+    endwhile;
+
     $this->logger->info($submission['url']);
     
+    //submit url to database    
     try {
         $this->db->set($id, ['url' => $submission['url'], 'time' => $time]);
     } catch(Exception $e) {
