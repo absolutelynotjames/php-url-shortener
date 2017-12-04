30c0b6953c758a833dc972ae5a3675043b8d8e60
James Myers <james.myers@msn.com>
2017-12-03 23:28:44 -0500

    route for /new

--- src/routes.php
+++ src/routes.php
@@ -12,3 +12,9 @@ $app->get('/[{name}]', function (Request $request, Response $response, array $ar
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
+
+$app->post('/new', function (Request $request, Response $response, array $args) {
+    // Sample log message
+    $this->logger->info($request->getBody());
+    // Render index view
+});
