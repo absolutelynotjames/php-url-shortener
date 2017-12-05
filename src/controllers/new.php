6c1d1aa71ce552622112026523eb50323942bfed
James Myers <james.myers@msn.com>
2017-12-05 04:43:32 -0500

    backend data validation

--- src/controllers/new.php
+++ src/controllers/new.php
@@ -19,23 +19,28 @@
     }
 
     public function normalizeUrl($url)
-    {        
+    {
+
+        if (!preg_match("/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/", $url)) {
+            return false;
+        }
+
         /*
         first check to see if our url starts with a protocol, if not assign https
-        https://stackoverflow.com/questions/2762061/how-to-add-http-if-it-doesnt-exists-in-the-url 
-        */
+        https://stackoverflow.com/questions/2762061/how-to-add-http-if-it-doesnt-exists-in-the-url
+         */
         if (!preg_match("~^(?:f|ht)tps?://~i", $url)) {
             $url = "https://" . $url;
-        }        
+        }
 
-        //parse url        
-        $parse = parse_url($url);          
-        
+        //parse url
+        $parse = parse_url($url);
+
         //if path is empty, set it to an empy string
         if (empty($parse['path'])) {
             $parse['path'] = '';
         }
-        
+
         //strip www from host if it exists
         $url = preg_replace('#^(http(s)?://)?w{3}\.#', '$1', $url);
 
@@ -51,28 +56,36 @@
 
         $requestBody['url'] = $this->normalizeUrl($requestBody['url']);
 
-        //generate inique id
-        $id = $this->idGenerator->generateString(8, $this->characters);
+        if ($requestBody['url']) {
+            $this->logger->info($requestBody['url'] == true);
 
-        //make sure id isn't already in the database
-        while ($this->db->get($id) !== false):
-            $id = $this->idGenerator->generateString(8, $this->$characters);
-        endwhile;
+            //generate inique id
+            $id = $this->idGenerator->generateString(8, $this->characters);
 
-        // build url with id
-        $url = $this->url . $id;
+            //make sure id isn't already in the database
+            while ($this->db->get($id) !== false):
+                $id = $this->idGenerator->generateString(8, $this->$characters);
+            endwhile;
 
-        //submit to database
-        $this->db->set($id, ['url' => $requestBody['url'], 'time' => $this->timestamp]);
+            // build url with id
+            $url = $this->url . $id;
 
-        $this->logger->info($url . ' submitted' . $requestBody['url']);
+            //submit to database
+            $this->db->set($id, ['url' => $requestBody['url'], 'time' => $this->timestamp]);
 
-        //build json
-        $responseBody->write(json_encode(array(
-            "url" => $url,
-        ), JSON_UNESCAPED_SLASHES));
-
+            $this->logger->info($url . ' submitted' . $requestBody['url']);
+            //build json
+            $responseBody->write(json_encode(array(
+                "url" => $url,
+            ), JSON_UNESCAPED_SLASHES));
+        }
+         else {
+            return $response
+            ->withStatus(500)
+            ->withHeader('Content-Type', 'text/html')
+            ->write('Not a valid url');
+        }
         //respond with the full url
-        return $response;
+        return $responseBody;
     }
 }
