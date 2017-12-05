e7bf03207f3db2dd18a57e879ff82519ac5602d0
James Myers <james.myers@msn.com>
2017-12-04 21:10:28 -0500

    new class cleanup

--- src/controllers/new.php
+++ src/controllers/new.php
@@ -6,43 +6,42 @@
     {
         $this->url = $url;        
         $this->db = $db;
-        $this->logger = $logger;        
+        $this->logger = $logger;       
+        $this->timestamp = Time();        
+        
+        //random number generator for unique id
+        $randomFactory = new RandomLib\Factory;
+        $this->idGenerator = $randomFactory->getLowStrengthGenerator();
+       
+        //let's keep our character set simple
+        $this->characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';      
+          
     }
     function new ($request, $response, $args) {
 
-        $body = $response->getBody();
+        $responseBody = $response->getBody();
 
         //get post body
-        $submission = $request->getParsedBody();
-
-        //random number generator for unique id
-        $randomFactory = new RandomLib\Factory;
-        $idGenerator = $randomFactory->getLowStrengthGenerator();
-
-        //let's keep our character set simple
-        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
-        //for timestamping submissions, might be useful for something later on
-        $time = Time();
+        $requestBody = $request->getParsedBody();
 
         //generate inique id
-        $id = $idGenerator->generateString(8, $characters);
+        $id = $this->idGenerator->generateString(8, $this->characters);
 
         //make sure id isn't already in the database
         while ($this->db->get($id) !== false):
-            $id = $idGenerator->generateString(8, $characters);
+            $id = $this->idGenerator->generateString(8, $this->$characters);
         endwhile;
 
         // build url with id
         $url = $this->url . $id;
 
         //submit to database
-        $this->db->set($id, ['url' => $submission['url'], 'time' => $time]);
+        $this->db->set($id, ['url' => $requestBody['url'], 'time' => $this->timestamp]);
 
         $this->logger->info($url . ' submitted');
 
         //build json
-        $body->write(json_encode(array(
+        $responseBody->write(json_encode(array(
             "url" => $url,
         ), JSON_UNESCAPED_SLASHES));
 
