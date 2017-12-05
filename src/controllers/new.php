11f652333ce770c45108fe1fef5c2b1b88dd508b
James Myers <james.myers@msn.com>
2017-12-04 23:58:28 -0500

    Function to normalize url before inserting

--- src/controllers/new.php
+++ src/controllers/new.php
@@ -4,25 +4,52 @@
 
     public function __construct($url, $db, $logger)
     {
-        $this->url = $url;        
+        $this->url = $url;
         $this->db = $db;
-        $this->logger = $logger;       
-        $this->timestamp = Time();        
-        
+        $this->logger = $logger;
+        $this->timestamp = Time();
+
         //random number generator for unique id
         $randomFactory = new RandomLib\Factory;
         $this->idGenerator = $randomFactory->getLowStrengthGenerator();
-       
+
         //let's keep our character set simple
-        $this->characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';      
-          
+        $this->characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
     }
+
+    public function normalizeUrl($url)
+    {        
+        /*
+        first check to see if our url starts with a protocol, if not assign https
+        https://stackoverflow.com/questions/2762061/how-to-add-http-if-it-doesnt-exists-in-the-url 
+        */
+        if (!preg_match("~^(?:f|ht)tps?://~i", $url)) {
+            $url = "https://" . $url;
+        }        
+
+        //parse url        
+        $parse = parse_url($url);          
+        
+        //if path is empty, set it to an empy string
+        if (empty($parse['path'])) {
+            $parse['path'] = '';
+        }
+        
+        //strip www from host if it exists
+        $parse['host'] = preg_replace('#^www\.(.+\.)#i', '$1', $parse['host']);  
+
+        return $url;
+    }
+
     function new ($request, $response, $args) {
 
         $responseBody = $response->getBody();
 
         //get post body
         $requestBody = $request->getParsedBody();
+
+        $requestBody['url'] = $this->normalizeUrl($requestBody['url']);
 
         //generate inique id
         $id = $this->idGenerator->generateString(8, $this->characters);
@@ -38,7 +65,7 @@
         //submit to database
         $this->db->set($id, ['url' => $requestBody['url'], 'time' => $this->timestamp]);
 
-        $this->logger->info($url . ' submitted');
+        $this->logger->info($url . ' submitted' . $requestBody['url']);
 
         //build json
         $responseBody->write(json_encode(array(
